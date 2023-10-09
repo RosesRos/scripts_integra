8efbfb05964033da441f193e86a428bcfcab6201
Rsesros <rostranespinoza@gmail.com>
2023-10-09 20:51:53 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 require "db.php";
 
 $subdomain     = '5wsigcmzgt'; // поддомен AmoCRM
-$client_secret = 'Gb9wKRtL7Et8mDNRZ4Qrvu0G8QB3rmckJa4Lm4NyVZZCPavA8LwnlQmXf8sdFkto'; // Секретный ключ
+$client_secret = 'xxx'; // Секретный ключ
 $client_id     = '95f76aae-1e1c-4db3-9613-a63764752d94'; // ID интеграции
 $code          = 'xxx'; // Код авторизации
 $token_file    = 'tokens.txt';
